82efd20ff74895e1b2d422184f2b758d68d48f10
Francisco Revilla <francisco.revilla@infarm.com>
2022-03-24 21:38:18 +0100

    Use serial for ids

--- migrations/2022-02-21-200858_slots_table/up.sql
+++ migrations/2022-02-21-200858_slots_table/up.sql
@@ -1,13 +1,13 @@
 -- Your SQL goes here
 CREATE TABLE bookable (
-  id integer PRIMARY KEY,
+  id serial PRIMARY KEY,
   name varchar NOT NULL
 );
 
 CREATE TABLE slots (
-  id integer PRIMARY KEY,
+  id serial PRIMARY KEY,
   state varchar NOT NULL,
   start bigint NOT NULL,
   finish bigint NOT NULL,
-  bookable integer REFERENCES bookable (id) ON DELETE RESTRICT NOT NULL
+  bookable_id integer REFERENCES bookable (id) ON DELETE RESTRICT NOT NULL
 );
